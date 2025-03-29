70111632f2de22c41accdc70a7818cdf3572566d
RaushanUm <raushan.umaralieva@mail.ru>
2025-03-29 23:38:27 +0400

    Lesson_1.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -69,6 +69,13 @@ class Lesson_2 {
         Task5();
         Task6();
         Task7();
+        Task8();
+        Task9();
+        Task10();
+        Task11();
+        Task12();
+        Task13();
+        Task14();
     }
 
     public static void Task5() {
@@ -99,9 +106,114 @@ class Lesson_2 {
     public static void Task7() {
         System.out.println("Задание 7");
         boolean isNegativeNumber = isNegative(-10);
-        System.out.println ("Число отрицательное:"  + isNegativeNumber);
+        System.out.println("Число отрицательное:" + isNegativeNumber);
     }
-    public static boolean isNegative (int number) {
+
+    public static boolean isNegative(int number) {
         return number < 0;
     }
+
+    public static void Task8() {
+        System.out.println("Задание 8");
+        String string = "Котенок";
+        int count = 3;
+        System.out.println("String:" + string.repeat(count));
+    }
+
+    public static void Task9() {
+        System.out.println("Задание 9");
+        boolean isLeap = isLeapYear(2025);
+        System.out.println("Високосный год " + isLeap);
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else return year % 4 == 0;
+    }
+
+    public static void Task10() {
+        System.out.println("Задание 10");
+        int[] arr = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+        }
+        for (int num : arr) {
+            System.out.println(num + " ");
+        }
+    }
+
+    public static void Task11() {
+        System.out.println("Задание 11");
+        int[] arr = emptyArr(100);
+        for (int num : arr) {
+            System.out.println(num + " ");
+        }
+    }
+
+    public static int[] emptyArr(int length) {
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            arr[i] = i + 1;
+        }
+        return arr;
+    }
+
+    public static void Task12() {
+        System.out.println("Задание 12");
+        int[] arr = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 6) {
+                arr[i] *= 2;
+            }
+        }
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public static void Task13() {
+        System.out.println("Задание 13");
+        int n = 4;
+        int[][] array = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            array[i][i] = 1;
+            array[i][n - i - 1] = 1;
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println( " ");
+        }
+    }
+
+    public static void Task14() {
+        System.out.println("Задание 14");
+        int len = 7;
+        int initialValue = 5;
+        int[] resultArr = createArr(len, initialValue);
+        for (int num : resultArr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+    public static int[] createArr(int len, int initialValue) {
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = initialValue;
+        }
+        return arr;
+    }
 }
+
+
+
+
+
